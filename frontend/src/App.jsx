e5ad998a70855c0e036ecefb59cc71ed08b8899d
Ubaid <ahmed.ubaid2001@gmail.com>
2024-10-18 21:22:16 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,49 +6,10 @@ import './App.css'
 
 function App() {
   
-  const [product, setProduct] = useState({
-    productId:'',
-    seller:'',
-    productName:'',
-    description:'',
-    quantity:0,
-    price:0,
-    category:''
-  })
-  const [error,setError]=useState('')
 
   const router=createBrowserRouter([
     {path:'/',element:<Add/>}
   ])
-
-
-  const handleChange=async(e)=>{
-    const { name, value } = e.target;
-
-    // If the input is a number field, ensure we store a number, not a string
-    setProduct({
-        ...product, 
-        [name]: name === 'quantity' || name==='price'? (value === '' ? '' : parseInt(value)) : value
-    })
-  }
-
-  const handleSubmit=async (e)=>{
-    e.preventDefault()
-    try{
-      await axios.post('http://localhost:200/addProduct',product)
-      setProduct({
-        productId:'',
-        seller:'',
-        productName:'',
-        description:'',
-        quantity:0,
-        price:0,
-        category:''
-      })
-    }catch(error){
-      setError('Invalid input')
-    }
-  }
 
 
   return (
